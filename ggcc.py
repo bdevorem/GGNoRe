810323d1e5a70881ce3084a7223455f4fa06b062
Breanna Devore-McDonald <bdevorem@gmail.com>
2019-10-23 15:04:44 -0400

    add copyright header

--- ggcc.py
+++ ggcc.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python2.7
 #naive implementation of a program that fixes gcc errors
 #uses https://github.com/thibauts/duckduckgo
+
+# Copyright (c) 2019 Breanna Devore-McDonald and John Westhoff.
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import urllib
 import string
 import sys
